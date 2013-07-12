3bb7bc1f50285d51eaa7fac3d64e2dd99b001ef4
Mile Milovanovic <Mile@Miles-MacBook-Air.local>
2013-07-12 12:22:53 -0500

    updated css

--- public/js/footer.js
+++ public/js/footer.js
@@ -2,11 +2,5 @@ $(document).ready(function(){
   $('#toggleOpen').click(function(){
     $('nav').toggle();
     $('#toggleClose').toggle();
- 
   });
-  $('#toggleClose').click(function(){
-    $('nav').toggle();
-    $('#toggleOpen').hide();
-    $('#toggleClose').hide();
-});
 });
